7621ecde779342bb41294d8ea6f061aeb52fd281
DowneyX <DowneyX@github.com>
2023-10-15 12:19:35 +0200

    added routes and models

--- app.py
+++ app.py
@@ -1,8 +1,87 @@
-from website import create_app, db
-from flask_migrate import Migrate
+#----------------------------------------------------------------------------#
+# Imports
+#----------------------------------------------------------------------------#
 
-app = create_app()
-migrate = Migrate(app, db)
+from flask import Flask, render_template, request
+# from flask.ext.sqlalchemy import SQLAlchemy
+import logging
+from logging import Formatter, FileHandler
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
+#----------------------------------------------------------------------------#
+# App Config.
+#----------------------------------------------------------------------------#
+
+app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+from models import WeatherData
+
+with app.app_context():
+	db.create_all()
+
+# Automatically tear down SQLAlchemy.
+'''
+@app.teardown_request
+def shutdown_session(exception=None):
+    db_session.remove()
+'''
+
+# Login required decorator.
+'''
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+'''
+
+
+#----------------------------------------------------------------------------#
+# Controllers.
+#----------------------------------------------------------------------------#
+from controllers.WeatherApiController import weather_data_api
+app.register_blueprint(weather_data_api, url_prefix='/api/weather_data')
+
+# Error handlers.
+@app.errorhandler(500)
+def internal_error(error):
+    #db_session.rollback()
+    return render_template('errors/500.html'), 500
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('errors/404.html'), 404
+
+if not app.debug:
+    file_handler = FileHandler('error.log')
+    file_handler.setFormatter(
+        Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+    )
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('errors')
+
+#----------------------------------------------------------------------------#
+# Launch.
+#----------------------------------------------------------------------------#
+
+# Default port:
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
+
+# Or specify port manually:
+'''
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+'''
---
+++ controllers/WeatherApiController.py
@@ -0,0 +1,123 @@
+from datetime import date, datetime
+from flask import Blueprint, flash, redirect, render_template, url_for, request, jsonify
+from models.WeatherData import WeatherData
+import json
+
+weather_data_api = Blueprint('weather_data_api', __name__)
+
+
+@weather_data_api.route('/post', methods=['POST'])
+def weatherData_post():
+    data = request.get_json(force=True)
+    if (not "device_id" in data):
+        return "bad request", 400
+    if (not "measured_at" in data):
+        return "bad request", 400
+    if (not "temperature" in data):
+        return "bad request", 400
+    if (not "pressure" in data):
+        return "bad request", 400
+    if (not "humidity" in data):
+        return "bad request", 400
+
+    data_obj = WeatherData(
+        temperature=data["temperature"],
+        pressure=data["pressure"],
+        humidity=data["humidity"],
+        measured_at=datetime.strptime(
+            data["measured_at"], "%d/%m/%Y, %H:%M:%S"),
+        has_send=False,
+        created_at=datetime.now()
+    )
+    db.session.add(data_obj)
+    db.session.commit()
+
+    return "data comitted"
+
+
+@weather_data_api.route('/get/<date>', methods=['GET'])
+def weather_data_get_date(date):
+
+    if not is_date(date):
+        return "Bad request", 400
+
+    try:
+        # Parse the date string into a datetime object
+        date_obj = datetime.strptime(date, "%Y-%m-%d")
+
+        # Calculate the start and end datetime objects for the specified day
+        start_datetime = datetime.combine(date_obj.date(), datetime.min.time())
+        end_datetime = datetime.combine(date_obj.date(), datetime.max.time())
+
+        # Query the database for weather data within the date range
+        weather_data = WeatherData.query.filter(
+            WeatherData.measured_at >= start_datetime,
+            WeatherData.measured_at <= end_datetime
+        ).all()
+
+        # Convert the queried data to JSON response or return an appropriate response
+        if weather_data:
+            data_as_dicts = [
+                {
+                    'id': data.id,
+                    'has_send': data.has_send,
+                    'measured_at': data.measured_at.strftime("%Y-%m-%d %H:%M:%S"),
+                    'temperature': data.temperature,
+                    'humidity': data.humidity,
+                    'pressure': data.pressure
+                }
+                for data in weather_data
+            ]
+            return jsonify(data_as_dicts)
+        else:
+            return "No data found for the specified date.", 404
+    except ValueError:
+        return "Bad request", 400
+
+
+@weather_data_api.route('/get/not-send/', methods=['GET'])
+def weather_data_get_not_send():
+    filtered_data = WeatherData.query.filter(WeatherData.has_send == False)
+    weather_data = filtered_data.limit(1000).all()
+
+    if weather_data:
+        data_as_dicts = [
+            {
+                'id': data.id,
+                'has_send': data.has_send,
+                'measured_at': data.measured_at.strftime("%Y-%m-%d %H:%M:%S"),
+                'temperature': data.temperature,
+                'humidity': data.humidity,
+                'pressure': data.pressure
+            }
+            for data in weather_data
+        ]
+        return jsonify(data_as_dicts)
+    else:
+        return "No unsend data.", 404
+
+@weather_data_api.route('/get/latest/', methods=['GET'])
+def weather_data_get_latest():
+    weather_data = WeatherData.query.order_by(WeatherData.measured_at.desc()).limit(1)
+    if weather_data:
+        data_as_dicts = [
+            {
+                'id': data.id,
+                'has_send': data.has_send,
+                'measured_at': data.measured_at.strftime("%Y-%m-%d %H:%M:%S"),
+                'temperature': data.temperature,
+                'humidity': data.humidity,
+                'pressure': data.pressure
+            }
+            for data in weather_data
+        ]
+        return jsonify(data_as_dicts)
+    else:
+        return "No unsend data.", 404
+
+def is_date(date_str):
+    try:
+        datetime.strptime(date_str, "%Y-%m-%d")
+        return True
+    except ValueError:
+        return False
